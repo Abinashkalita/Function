4f7e5e936dbe4865a9477f5e05d39e1ff52690d0
Abinashkalita <111621458+Abinashkalita@users.noreply.github.com>
2022-11-06 02:03:01 +0530

    1st commit

--- 8_fibonaaci.c
+++ 8_fibonaaci.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+int fibonaaci(int num)
+{
+    //base conditions
+    //num==0   ------->return 0
+    //num==1   ------->return 1
+    if (num==0){
+        return 0;
+    }
+    else if (num==1){
+        return 1;
+    }
+    else{
+        return fibonaaci(num-1) + fibonaaci(num-2);
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    printf("Fibonaaci series of %dth number is %d",num,fibonaaci(num));
+    return 0;
+}
